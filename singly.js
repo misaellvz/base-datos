7a444fa1abd5d54631d791a7bf7d55d7ec540aba
Misael Valdez <misael.valdez994@gmail.com>
2021-01-14 16:52:14 -0300

    Se agregó el metodo insert

--- singly.js
+++ singly.js
@@ -32,6 +32,29 @@ class MySinglyLinkedList {
       this.length++;
       return this;
   }
+  insert(index, value) {
+    if(index >= this.length){
+      return this.append(value)
+    }
+
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index -1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  getTheIndex(index){
+    let counter = 0;
+    let currentNode = this.head;
+
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
